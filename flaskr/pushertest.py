79c6721e9bc41324848ef6852f4a0345c0d23868
Ashley Lorden <ashley.f.lorden@gmail.com>
2012-07-26 17:15:06 -0700

    working screensharing

--- flaskr/pushertest.py
+++ flaskr/pushertest.py
@@ -7,30 +7,26 @@ app.config.from_object(__name__)
 app.config.from_envvar("FLASKR_SETTINGS", silent = True)
 
 
-pusher_application_key = config.pusher_application_key
 pusher.app_id = config.pusher_app_id
 pusher.key = config.pusher_key
 pusher.secret = config.pusher_secret
 
-p = pusher.Pusher()
+@app.before_request
+def setup_function():
+    g.pusher = pusher.Pusher()
 
 
 @app.route ("/")
 def home():
-	return render_template("pushertest.html", key = pusher.key, app_id = pusher.app_id)
+	return render_template("screentest.html")
 
-@app.route("/")
-def url_route():
-	url = request.form("URL")
-	print url
-
-
-#p["browser_channel"].trigger("url_change", {"hola": "gato"})
-
-#def main():
-
+@app.route("/navigate")
+def navigate():
+    dest_url = request.args['destination_url']
+    g.pusher['channel_name'].trigger("URL_change", {"url": dest_url})
+    return ""
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
