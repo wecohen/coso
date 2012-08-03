f8d9be22a61972c700f4606845f6cb15acc7b84b
Student <student@Administrators-Mac-mini-5.local>
2012-08-03 14:07:18 -0700

    Updating git ignore, adding config template

---
+++ flaskr/config_template.py
@@ -0,0 +1,12 @@
+pusher_application_key = 'PUSHER APP KEY'
+pusher_app_id = 'PUSHER APP ID'
+pusher_key = 'PUSHER KEY'
+pusher_secret = 'PUSHER SECRET'
+
+OT_apiKey = "OPENTOK API KEY" # Replace with your API key. See http://www.tokbox.com/
+OT_sessionID = "OPENTOK SESSION ID" # Replace with your own session ID.
+                    # See http://www.tokbox.com/opentok/api/tools/generator
+OT_token = "OPENTOK DEV TOKEN" # Replace with a generated token that has been assigned the moderator role.
+                # See http://www.tokbox.com/opentok/api/tools/generator
+
+thing = "This is the thing"
