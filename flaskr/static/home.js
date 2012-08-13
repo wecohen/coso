4c822f7cb1314e31164f10c4e05b6f146a967985
wecohen <we.cohen@gmail.com>
2012-08-13 16:17:09 -0700

    aug 13th additions

--- flaskr/static/home.js
+++ flaskr/static/home.js
@@ -2,11 +2,39 @@ var showAbout = function() {
 	$("#about_text").slideToggle(600);
 };
 
+var showInvite = function() {
+	$("#invite_text").slideToggle(200);
+}
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
+	if(!d.getElementById(id)){js=d.createElement(s);
+		js.id=id;
+		js.src="//platform.twitter.com/widgets.js";
+		fjs.parentNode.insertBefore(js,fjs);}}
+		(document,"script","twitter-wjs");
+
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}
+(document, 'script', 'facebook-jssdk'));
+
 var main = function() {
 	$("#about_text").hide();
+	$("#invite_text").hide();
+	$("#topright").click(function(){
+		showInvite()
+	});
 	$("#about").click(function(){
 		showAbout()
+
 	});
+
 };
+
 
 $(document).ready(main);
