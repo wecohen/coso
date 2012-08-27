e7959ee891ccf7d8b170ff57c84b4e90b76c467e
Ashley Lorden <ashley.f.lorden@gmail.com>
2012-08-27 09:30:48 -0700

    think I fixed config file for Heroku

--- flaskr/config.py
+++ flaskr/config.py
@@ -2,19 +2,15 @@ import os
 
 pusher_application_key = 'b31d655fa7f11bd6f11d'
 pusher_app_id = '24605'
-pusher_key = os.getenv("PUSHER_KEY", "")
-pusher_secret = os.getenv("PUSHER_SECRET", "")
+pusher_key = os.getenv("pusher_key", "")
+pusher_secret = os.getenv("pusher_secret", "")
 
-OT_apiKey = os.getenv("OT_KEY", "")
+OT_apiKey = os.getenv("OT_apiKey", "")
 OT_sessionID = "2_MX4xNjgwMTQxMn5-MjAxMi0wNy0yNCAyMjo1OToxOC4zNDQyODkrMDA6MDB-MC4xNTQyODA0MzQxODJ-" # Replace with your own session ID.
                     # See http://www.tokbox.com/opentok/api/tools/generator
 OT_token = "devtoken" # Replace with a generated token that has been assigned the moderator role.
                 # See http://www.tokbox.com/opentok/api/tools/generator
-OT_apiSecret = os.getenv("OT_SECRET", "")
-
-thing = "This is the thing"
-
-print OT_apiSecret
+OT_apiSecret = os.getenv("OT_apiSecret", "")
 
 #try:
 #  from config_local import *
